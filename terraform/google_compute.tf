9321513d2806a3f5aaa3d648c93286b399bd85bf
sfujiwara <shuhei.fujiwara@gmail.com>
2025-01-03 03:20:56 +0900

    https redirect

--- terraform/google_compute.tf
+++ terraform/google_compute.tf
@@ -41,12 +41,27 @@ resource "google_compute_url_map" "default" {
   default_service = google_compute_backend_service.default.id
 }
 
+resource "google_compute_url_map" "https_redirect" {
+  name            = "${var.name}-https-redirect"
+
+  default_url_redirect {
+    https_redirect         = true
+    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"
+    strip_query            = false
+  }
+}
+
 resource "google_compute_target_https_proxy" "default" {
-  name    = var.name
+  name    = "${var.name}-https-proxy"
   url_map = google_compute_url_map.default.id
   ssl_certificates = [
     google_compute_managed_ssl_certificate.default.id
   ]
+}
+
+resource "google_compute_target_http_proxy" "https_redirect" {
+  name   = "${var.name}-http-proxy"
+  url_map          = google_compute_url_map.https_redirect.id
 }
 
 resource "google_compute_global_forwarding_rule" "default" {
@@ -54,5 +69,13 @@ resource "google_compute_global_forwarding_rule" "default" {
   target                = google_compute_target_https_proxy.default.id
   port_range            = "443"
   ip_address            = google_compute_global_address.default.address
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+}
+
+resource "google_compute_global_forwarding_rule" "https_redirect" {
+  name   = "${var.name}-https-redirect"
+  target = google_compute_target_http_proxy.https_redirect.id
+  port_range = "80"
+  ip_address = google_compute_global_address.default.address
   load_balancing_scheme = "EXTERNAL_MANAGED"
 }
