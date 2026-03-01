7650791dce388c176372e5789c4af9a008d9cc43
sfujiwara <shuhei.fujiwara@gmail.com>
2026-03-01 18:39:16 +0900

    rum

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import './App.css'
+import "./App.css";
 
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
 
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-
-import ListProfileItem from './ListProfileItems';
+import ListProfileItem from "./ListProfileItems";
 import {
   academicConferences,
   accounts,
@@ -19,57 +18,137 @@ import {
   superComputers,
   technicalPapers,
   workExperiences,
-} from './items';
-
+} from "./items";
 
 function App() {
-
   return (
     <>
-      <Typography component="h1" variant="h3" gutterBottom>Shuhei Fujiwara</Typography>
-      <List>{accounts.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h1" variant="h3" gutterBottom>
+        Shuhei Fujiwara
+      </Typography>
+      <List>
+        {accounts.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h2" variant="h4" gutterBottom>Work Experience</Typography>
-      <List>{workExperiences.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h2" variant="h4" gutterBottom>
+        Work Experience
+      </Typography>
+      <List>
+        {workExperiences.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h2" variant="h4" gutterBottom>Education</Typography>
-      <List>{educations.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h2" variant="h4" gutterBottom>
+        Education
+      </Typography>
+      <List>
+        {educations.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h2" variant="h4" gutterBottom>Publications</Typography>
+      <Typography component="h2" variant="h4" gutterBottom>
+        Publications
+      </Typography>
 
-      <Typography component="h3" variant="h5" gutterBottom>Books</Typography>
-      <List>{books.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h3" variant="h5" gutterBottom>
+        Books
+      </Typography>
+      <List>
+        {books.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h3" variant="h5" gutterBottom>Refereed Journals</Typography>
-      <List>{journals.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h3" variant="h5" gutterBottom>
+        Refereed Journals
+      </Typography>
+      <List>
+        {journals.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h3" variant="h5" gutterBottom>Refereed Conference Proceedings</Typography>
-      <List>{conferenceProceedings.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h3" variant="h5" gutterBottom>
+        Refereed Conference Proceedings
+      </Typography>
+      <List>
+        {conferenceProceedings.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h3" variant="h5" gutterBottom>Technical Papers</Typography>
-      <List>{technicalPapers.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h3" variant="h5" gutterBottom>
+        Technical Papers
+      </Typography>
+      <List>
+        {technicalPapers.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h2" variant="h4" gutterBottom>Talks</Typography>
+      <Typography component="h2" variant="h4" gutterBottom>
+        Talks
+      </Typography>
 
-      <Typography component="h3" variant="h5" gutterBottom>Academic Conferences</Typography>
-      <List>{academicConferences.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h3" variant="h5" gutterBottom>
+        Academic Conferences
+      </Typography>
+      <List>
+        {academicConferences.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h3" variant="h5" gutterBottom>Tutor of Super Computer Seminars</Typography>
-      <List>{superComputers.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h3" variant="h5" gutterBottom>
+        Tutor of Super Computer Seminars
+      </Typography>
+      <List>
+        {superComputers.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h3" variant="h5" gutterBottom>Selected Talks in Tech Events</Typography>
-      <List>{selectedTalks.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h3" variant="h5" gutterBottom>
+        Selected Talks in Tech Events
+      </Typography>
+      <List>
+        {selectedTalks.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h2" variant="h4" gutterBottom>Awards</Typography>
-      <List>{awards.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h2" variant="h4" gutterBottom>
+        Awards
+      </Typography>
+      <List>
+        {awards.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h2" variant="h4" gutterBottom>Activities</Typography>
-      <List>{activities.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h2" variant="h4" gutterBottom>
+        Activities
+      </Typography>
+      <List>
+        {activities.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
 
-      <Typography component="h2" variant="h4" gutterBottom>Certifications</Typography>
-      <List>{certifications.map(item => <ListProfileItem text={item.text} href={item.link} icon={item.icon} />)}</List>
+      <Typography component="h2" variant="h4" gutterBottom>
+        Certifications
+      </Typography>
+      <List>
+        {certifications.map((item) => (
+          <ListProfileItem text={item.text} href={item.link} icon={item.icon} />
+        ))}
+      </List>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { datadogRum } from '@datadog/browser-rum';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { datadogRum } from "@datadog/browser-rum";
+import { datadogLogs } from "@datadog/browser-logs";
 
 datadogRum.init({
-    applicationId: '8f6be096-fe15-4de7-b7df-f696cc9ef3a1',
-    clientToken: 'pubbb5f48079df1b286511ffda958c56250',
-    site: 'datadoghq.com',
-    service: 'sfujiwara.net',
-    env: 'production',
-    version: '1.0.0',
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 20,
-    trackBfcacheViews: true,
-    defaultPrivacyLevel: 'mask-user-input',
+  applicationId: "8f6be096-fe15-4de7-b7df-f696cc9ef3a1",
+  clientToken: "pubbb5f48079df1b286511ffda958c56250",
+  site: "datadoghq.com",
+  service: "sfujiwara.net",
+  env: "production",
+  version: "1.0.0",
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 10,
+  trackBfcacheViews: true,
+  defaultPrivacyLevel: "mask-user-input",
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+datadogLogs.init({
+  clientToken: "pubbb5f48079df1b286511ffda958c56250",
+  site: "datadoghq.com",
+  forwardErrorsToLogs: true,
+  sessionSampleRate: 100,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
